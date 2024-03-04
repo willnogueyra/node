828cae6d3b2350047653984c23ce54d38c9717ea
William Nogueira <will.ribeiro@live.com>
2024-03-04 22:41:23 +0000

    estrutura da aplicação

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import http from 'http';
+import http from "http";
 
 /* 
   - Criar usuarios
@@ -19,21 +19,27 @@ import http from 'http';
     - DELETE -> Deletar um recurso no back-end
 
 */
+const users = [];
 
 const server = http.createServer((req, res) => {
-  const { method, url } = req
+  const { method, url } = req;
 
-  if (method == 'GET' && url == '/users') {
-    return res.end('Listagem de usuarios');
+  if (method == "GET" && url == "/users") {
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
-  if (method == 'POST' && url == '/users') {
-    return res.end('Criação de usuarios')
+  if (method == "POST" && url == "/users") {
+    users.push({
+      id: 1,
+      name: "William",
+      email: "wl@examlple.com",
+    });
+    return res.writeHead(201).end();
   }
 
-  return res.end('hello WORLD')
-
-
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
